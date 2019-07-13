65c11db82072b5ba9104ef9672b3fbf6eefb7750
Matthew Rahtz <matthew.rahtz@gmail.com>
2019-07-13 12:58:54 +0200

    Fix tests

--- tests/tests.py
+++ tests/tests.py
@@ -62,7 +62,8 @@ class UnitTests(unittest.TestCase):
     def _get_model_with_polyak_coef(obs_dim, polyak_coef, seed=0):
         model = SACModel(obs_dim=obs_dim, n_actions=2, seed=seed, discount=0.99,
                          temperature=1e-3, polyak_coef=polyak_coef, lr=1e-3,
-                         act_lim=np.array([1, 1]), std_min_max=[1e-4, 4])
+                         act_lim=np.array([1, 1]), std_min_max=[1e-4, 4],
+                         network=[(256, 'relu'), (256, 'relu')])
         return model
 
     @staticmethod
@@ -143,7 +144,8 @@ class UnitTests(unittest.TestCase):
     @staticmethod
     def _get_policy_outputs(obs):
         obs_dim = obs.shape[1]
-        policy = TanhDiagonalGaussianPolicy(n_actions=3, act_lim=np.array([1, 1, 1]), std_min_max=(0.1, 1.0))
+        policy = TanhDiagonalGaussianPolicy(n_actions=3, act_lim=np.array([1, 1, 1]), std_min_max=(0.1, 1.0),
+                                            network=[(256, 'relu'), (256, 'relu')])
         obs_ph = tf.placeholder(tf.float32, [None, obs_dim])
         policy_ops = policy(obs_ph)
         sess = tf.Session()
@@ -192,7 +194,8 @@ class UnitTests(unittest.TestCase):
 
     @staticmethod
     def _get_act_limit_ops(act_lim):
-        policy = TanhDiagonalGaussianPolicy(n_actions=2, act_lim=act_lim, std_min_max=(0.1, 1.0))
+        policy = TanhDiagonalGaussianPolicy(n_actions=2, act_lim=act_lim, std_min_max=(0.1, 1.0),
+                                            network=[(256, 'relu'), (256, 'relu')])
         obs_ph = tf.placeholder(tf.float32, shape=[None, 1])
         p = policy(obs_ph)
         mean_op, log_std_op, pi_op = p.raw_mean, p.log_std, p.pi
