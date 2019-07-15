ea26588473f6addbc9bf2ddea7eff3db23a5f233
Matthew Rahtz <matthew.rahtz@gmail.com>
2019-07-15 08:57:33 +0200

    Log reward every n test episodes

--- sac/train.py
+++ sac/train.py
@@ -1,13 +1,14 @@
 import multiprocessing
 import os
+import sys
 
 import easy_tf_log
+import gym
 import numpy as np
 from sacred import Experiment
 from sacred.observers import FileStorageObserver
 
 import sac.config
-from sac.env import make_env
 from sac.model import SACModel
 from sac.replay_buffer import ReplayBuffer
 from sac.utils import tf_disable_warnings, tf_disable_deprecation_warnings, RateMeasure
@@ -26,7 +27,7 @@ def train_sac(buffer: ReplayBuffer, model: SACModel, train_env, test_env, n_star
               checkpoint_every_n_steps, train_n_steps, test_every_n_steps, batch_size):
     obs1, done = train_env.reset(), False
     logger = easy_tf_log.Logger(os.path.join(observer.dir, 'sac'))
-    n_steps = 0
+    episode_reward, n_steps = 0, 0
     step_rate_measure = RateMeasure(n_steps)
     losses = []
 
@@ -37,9 +38,12 @@ def train_sac(buffer: ReplayBuffer, model: SACModel, train_env, test_env, n_star
             act = model.step(obs1, deterministic=False)
 
         obs2, reward, done, _ = train_env.step(act)
+        episode_reward += reward
         buffer.store(obs1=obs1, acts=act, rews=reward, obs2=obs2, done=float(done))
         obs1 = obs2
         if done:
+            logger.logkv('env_train/reward', episode_reward)
+            episode_reward = 0
             obs1, done = train_env.reset(), False
 
         if len(buffer) < n_start_env_steps:
@@ -59,7 +63,7 @@ def train_sac(buffer: ReplayBuffer, model: SACModel, train_env, test_env, n_star
             losses = []
 
         if n_steps % test_every_n_steps == 0:
-            run_test_episodes(model, test_env, n_episodes=10, render=False)
+            run_test_episodes(model, test_env, n_episodes=10, logger=logger, render=False)
 
         if n_steps % checkpoint_every_n_steps == 0:
             model.save()
@@ -67,28 +71,40 @@ def train_sac(buffer: ReplayBuffer, model: SACModel, train_env, test_env, n_star
         n_steps += 1
 
 
-def run_test_episodes(model, env, n_episodes, render):
+def run_test_episodes(model, env, n_episodes, logger, render):
+    episodes_reward = []
     for _ in range(n_episodes):
         obs, done, = env.reset(), False
+        episode_reward = 0
         while not done:
             action = model.step(obs, deterministic=True)
             obs, reward, done, _ = env.step(action)
+            episode_reward += reward
             if render:
                 env.render()
+        episodes_reward.append(episode_reward)
+    mean_reward = np.mean(episodes_reward)
+    print("Test episodes done; mean reward {:.1f}".format(mean_reward))
+    sys.stdout.flush()
+    logger.logkv('env_test/episode_reward', mean_reward)
 
 
 def async_test_episodes_loop(model, model_load_dir, env_id, seed, log_dir):
-    env = make_env(env_id, seed, log_dir, 'test_async')
+    env = gym.make(env_id)
+    env.seed(seed)
+    logger = easy_tf_log.Logger(log_dir)
     while True:
         model.load(model_load_dir)
-        run_test_episodes(model, env, n_episodes=1, render=True)
+        run_test_episodes(model, env, n_episodes=1, logger=logger, render=True)
 
 
 @ex.automain
 def main(gamma, buffer_size, lr, seed, env_id, polyak_coef, temperature, policy_std_min, policy_std_max,
          async_test, network):
-    train_env = make_env(env_id, seed, observer.dir, 'train')
-    test_env = make_env(env_id, seed, observer.dir, 'test')
+    train_env = gym.make(env_id)
+    test_env = gym.make(env_id)
+    train_env.seed(seed)
+    test_env.seed(seed)
 
     buffer = ReplayBuffer(train_env.observation_space.shape, train_env.action_space.shape, max_size=buffer_size)
     obs_dim = train_env.observation_space.shape[0]
